fb7fe5b3a4c253293cbb33b038b975373584178c
nick.chen <nick.chen@fugle.tw>
2023-04-23 13:02:11 +0800

    add: MSwitch  components

---
+++ app/src/main/java/com/example/stockexplorer/ui/components/MSwitch.kt
@@ -0,0 +1,48 @@
+package com.example.stockexplorer.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.stockexplorer.ui.theme.Blue70
+
+@Composable
+fun MSwitch(
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+) {
+    Switch(
+        checked = checked,
+        onCheckedChange = onCheckedChange,
+        modifier = Modifier.size(width = 52.dp, height = 32.dp),
+        colors = SwitchDefaults.colors(
+            checkedThumbColor = Color.White,
+            checkedTrackColor = Blue70,
+            uncheckedThumbColor = Color.White,
+            uncheckedTrackColor = Color(0xFFF0F0F0), // FIXME: 顏色未定義在 DesignSystem
+            uncheckedBorderColor = Color(0xFFF0F0F0), // FIXME: 待確認顏色
+        ),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Column {
+        MSwitch(
+            checked = true,
+            onCheckedChange = {},
+        )
+        Box(modifier = Modifier.size(height = 10.dp, width = 0.dp))
+        MSwitch(
+            checked = false,
+            onCheckedChange = {},
+        )
+    }
+}
