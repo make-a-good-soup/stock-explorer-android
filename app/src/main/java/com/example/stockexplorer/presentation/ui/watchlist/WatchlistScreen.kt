f4d931ad8d15d8d383b4d9c76fdd212d6a48bc39
nick.chen <nick45chen@gmail.com>
2023-07-17 23:43:04 +0800

    add: WatchlistScreen placeholder.

--- app/src/main/java/com/example/stockexplorer/presentation/ui/watchlist/WatchlistScreen.kt
+++ app/src/main/java/com/example/stockexplorer/presentation/ui/watchlist/WatchlistScreen.kt
@@ -1,29 +1,100 @@
 package com.example.stockexplorer.presentation.ui.watchlist
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WatchlistScreen() {
     // TODO: Implement WatchlistScreen
+
+    var text by remember { mutableStateOf("") }
+    var selectedTabIndex by remember { mutableStateOf(0) }
+    val titles = listOf("全部", "半導體", "金融", "電子")
+
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text("追蹤清單") }
-            )
+            Row(modifier = Modifier.fillMaxWidth()) {
+                OutlinedTextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    label = { Text("Search") },
+                    modifier = Modifier.weight(1f) // this will make the TextField fill the remaining space
+                )
+                IconButton(onClick = { /* Handle settings click */ }) {
+                    Icon(Icons.Filled.Settings, contentDescription = "Settings")
+                }
+            }
         }
     ) { innerPadding ->
-        Text(
-            text = "Hello, Jetpack Compose!",
-            modifier = Modifier.padding(innerPadding)
-        )
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .height(30.dp)
+            ) {
+                ScrollableTabRow(
+                    selectedTabIndex = selectedTabIndex,
+                    modifier = Modifier.weight(1f)
+                ) {
+                    titles.forEachIndexed { index, title ->
+                        Tab(
+                            selected = selectedTabIndex == index,
+                            onClick = { selectedTabIndex = index }) {
+                            Text(title)
+                        }
+                    }
+                }
+                IconButton(onClick = { /* Handle settings click */ }) {
+                    Icon(Icons.Filled.Settings, contentDescription = "Settings")
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(text = "全部清單")
+                Spacer(modifier = Modifier.weight(1f))
+                Text(text = "最新價")
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(text = "漲跌幅")
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("This is the content for Tab ${titles[selectedTabIndex]}")
+            }
+        }
     }
 }
 
